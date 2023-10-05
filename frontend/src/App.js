253f44bf7fe8dadc6b1f5c460e8b307522c347fb
Usama Javed <usama.javaid1919@gmail.com>
2023-10-05 22:38:55 +0500

    initialized project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import classes from "./App.module.css";
+
+function App() {
+  return (
+    <div className={classes.app}>
+      <h1 className={classes.title}>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
