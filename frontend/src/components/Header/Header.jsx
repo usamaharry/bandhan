0dfea783e3d561b705a4bc5abf4b346e5ff01fd8
Usama Javed <usama.javaid1919@gmail.com>
2023-10-09 02:12:35 +0500

    modified brand name

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,24 +1,27 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
+//local
+import classes from "./Header.module.css";
+
 const Header = () => {
   return (
     <Navbar bg="light" variant="light" expand="md" collapseOnSelect>
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand>Bandhan</Navbar.Brand>
+          <Navbar.Brand className={classes.brand}>Bandhan</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav className="ms-auto">
             <LinkContainer to="/">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link>Home</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/contact">
-              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link>Contact</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/auth">
-              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link>Login</Nav.Link>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
